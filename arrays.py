662577fa8ee06f3b8d470bd415e90970fef331f4
Nilu Isakova <niluisakova@gmail.com>
2024-04-21 19:07:48 -0500

    added function to check arraylist

--- arrays.py
+++ arrays.py
@@ -1,9 +1,21 @@
 def check_if_array_is_sorted(arr_list):
     if arr_list == sorted(arr_list):
         print('The given array is sorted')
+        return 1
     else:   
-        print('The given array is not sorted')  
+        print('The given array is not sorted')
+        return 0
         
-check_if_array_is_sorted([1,2,3,4,5])
+print(check_if_array_is_sorted([1,2,3,4,5]))
 print('*****')
-check_if_array_is_sorted([4,2,3,4,5])
+print(check_if_array_is_sorted([4,2,3,4,5]))
+
+def max_value_in_array(arr_list):
+    max_value = arr_list[0]
+
+    for elem in range(1, len(arr_list)):
+        if arr_list[elem] > max_value:
+            max_value = arr_list[elem]
+    return max_value
+
+print(max_value_in_array([3,5,6,777]))
